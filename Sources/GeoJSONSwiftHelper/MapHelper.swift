79d1193dbbd591d29a1b902d1a7e03aa738cb0d4
asheshp23 <asheshp23@gmail.com>
2024-11-22 21:19:16 -0500

    minor changes

--- Sources/GeoJSONSwiftHelper/MapHelper.swift
+++ Sources/GeoJSONSwiftHelper/MapHelper.swift
@@ -32,19 +32,19 @@ extension CLLocationCoordinate2D {
   }
 }
 
-extension MKCoordinateRegion: Equatable {
+extension MKCoordinateRegion: @retroactive Equatable {
   public static func == (lhs: MKCoordinateRegion, rhs: MKCoordinateRegion) -> Bool {
     return lhs.center == rhs.center && lhs.span == rhs.span
   }
 }
 
-extension CLLocationCoordinate2D: Comparable {
+extension CLLocationCoordinate2D: @retroactive Comparable {
   public static func < (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
     return lhs.latitude < rhs.latitude && lhs.longitude < rhs.longitude
   }
 }
 
-extension MKCoordinateSpan : Equatable {
+extension MKCoordinateSpan : @retroactive Equatable {
   public static func == (lhs: MKCoordinateSpan, rhs: MKCoordinateSpan) -> Bool {
     return lhs.latitudeDelta == rhs.latitudeDelta && lhs.longitudeDelta == rhs.longitudeDelta
   }
